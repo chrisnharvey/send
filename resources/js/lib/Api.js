8d642333e5e6ba82dbef33f8f09b7f86557a6763
Chris Harvey <chris@chrisnharvey.com>
2018-11-27 21:09:07 +0000

    Add getFile and downloadFile to Api lib

--- resources/js/lib/Api.js
+++ resources/js/lib/Api.js
@@ -2,6 +2,33 @@ import Axios from "axios";
 
 export default class Api {
 
+  getFile(identifier, authKey) {
+    return new Promise((resolve, reject) => {
+      Axios.get(`/api/files/${identifier}?auth_key=${authKey}`).then(response => {
+        resolve(response.data.data)
+      }, response => {
+        reject(response)
+      })
+    })
+  }
+
+  downloadFile(path, onProgress) {
+    return new Promise((resolve, reject) => {
+      Axios.get(path, {
+        responseType: 'arraybuffer',
+        onDownloadProgress: (e) => {
+          if (! onProgress) {
+            return;
+          }
+
+          onProgress(
+            (Math.floor((e.loaded / e.total) * 100))
+          )
+        }
+      }).then(response => resolve(response.data))
+    })
+  }
+
 	uploadFile(name, file, authKey) {
 
 		// Instantiate a new FormData object
