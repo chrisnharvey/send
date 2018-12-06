99097d5f623283f8f006e1f3c6dd25de13420790
Chris Harvey <chris@chrisnharvey.com>
2018-12-06 22:48:22 +0000

    Add some intro to Upload Complete page

--- resources/js/components/UploadSuccess.js
+++ resources/js/components/UploadSuccess.js
@@ -4,7 +4,8 @@ export default class UploadSuccess extends Component {
   render() {
     return (
       <div>
-        <h2>Upload complete</h2>
+        <h1 style={{textAlign: 'center'}}>Upload Complete</h1>
+        <p style={{textAlign: 'center'}}>Your file has been uploaded successfully and will expire in 48 hours. You can share your file using the link below.</p>
 
         <p>URL: http://send.local/{this.props.data.identifier}#k={this.props.data.key}&s={this.props.data.salt}</p>
       </div>
